5e1df83f9d9ca132278e4f66845ea8d5cd9a1c1c
eboella <elisabetta.boella@gmail.com>
2021-08-22 15:43:04 +0200

    New definition of double portable across platforms; check on memory allocation.

--- borisPusher.f90
+++ borisPusher.f90
@@ -1,43 +1,94 @@
 program BorisPusher
 
-use m_units
-
 implicit none
 
+! Define double precision working with all compilers
+integer, parameter :: rp = selected_real_kind(15)
 
 ! Position 
-integer :: x = 0
-integer :: y = 0 
-integer :: z = 0
+real(rp) :: x 
+real(rp) :: y 
+real(rp) :: z
 
-double precision, parameter :: vx = 0.0
-double precision, parameter :: vy = 0.0 
-double precision, parameter :: vz = 0.0
+real(rp) :: vx 
+real(rp) :: vy 
+real(rp) :: vz
 
-double precision, parameter :: Ex = 0.0
-double precision, parameter :: Ey = 0.0 
-double precision, parameter :: Ez = 0.0
+real(rp) :: Ex 
+real(rp) :: Ey 
+real(rp) :: Ez 
 
-double precision, parameter :: Bx = 0.0
-double precision, parameter :: By = 0.0 
-double precision, parameter :: Bz = 0.1
+real(rp) :: Bx 
+real(rp) :: By  
+real(rp) :: Bz
 
+real(rp) :: v2, g, ux, uy, uz, ux_min, uy_min, uz_min 
+real(rp) :: tx, ty, tz, t2, sx, sy, sz
+real(rp) :: ux_pr, uy_pr, uz_pr, ux_plus, uy_plus, uz_plus
+real(rp) :: u2, gamma, x1, y1, z1
 
- real v2, g, ux, uy, uz, ux_min, uy_min, uz_min 
- integer :: tx, ty, tz, t2, sx, sy, sz
- integer :: ux_pr, uy_pr, uz_pr, ux_plus, uy_plus, uz_plus
- integer :: u2, gamma, x1, y1, z1
- integer :: i
+integer :: i
+integer :: ierr
 
-integer :: Nt = 10000
-integer :: dt = 0.1
+integer :: Nt
+real(rp) :: dt
 
-real(kind=8), allocatable :: T(:), XX(:), YY(:), ZZ(:), UUX(:), UUY(:), UUZ(:)
+real(rp) :: qom, moq
+
+real(rp), dimension(:), allocatable :: T, XX, YY, ZZ, UUX, UUY, UUZ
+
+namelist /input_file/ moq, Nt, dt, x, y, z, vx, vy, vz, Ex, Ey, Ez, Bx, By, Bz  
+
+!======================================================================
+!======================================================================
+!======================================================================
+!======================================================================
 
 ! Allocate memory
-allocate( T(Nt), XX(Nt), YY(Nt), ZZ(Nt), UUX(Nt), UUY(Nt), UUZ(Nt) )
+allocate(T(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array T'
+   stop
+end if
 
-! print*, 'after allocation'
+allocate(XX(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array XX'
+   stop
+end if
+
+allocate(YY(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array YY'
+   stop
+end if
+
+allocate(ZZ(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array ZZ'
+   stop
+end if
+
+allocate(UUX(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array UUX'
+   stop
+end if
+
+allocate(UUY(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array UUY'
+   stop
+end if
+
+allocate(UUZ(Nt), stat=ierr)
+if (ierr /= 0) then
+   print*,  'Failing to allocate memory for array UUZ'
+   stop
+end if
+
+print*, 'after allocation'
+
 v2 = vx**2 + vy**2 + vz**2
 g  = 1 / (1-v2)**0.5
 
@@ -45,18 +96,13 @@ ux = g*vx
 uy = g*vy 
 uz = g*vz
 
-do i = 1, Nt
-    print*, i
-    T(i)   = i*0.0
-    XX(i)  = i*0.0
-    YY(i)  = i*0.0
-    ZZ(i)  = i*0.0
-    UUX(i) = i*0.0
-    UUY(i) = i*0.0
-    UUZ(i) = i*0.0
- end do
+T = 0.0_dp
+XX = 0.0_dp
+YY = 0.0_dp
+UUX = 0.0_dp
+UUY = 0.0_dp
+UUZ = 0.0_dp
 
-T(1)   = 0
 XX(1)  = x 
 YY(1)  = y 
 ZZ(1)  = z
@@ -69,55 +115,48 @@ do i = 2, Nt
 
    print*, i
 
-   ux_min = ux + (q/me)* Ex*dt/2
-   uy_min = uy
-   uz_min = uz
+   ux_min = ux + qom * Ex * dt/2
+   uy_min = uy + qom * Ey * dt/2
+   uz_min = uz + qom * Ez * dt/2
 
    tx = 0
    ty = 0
-   tz = - (q/(me*c))* Bz * dt/(2*g)
+   tz = - (qom*(1/c))* Bz * dt/(2*g)
    t2 = (tx**2 + ty**2 + tz**2)
 
    sx = 2*tx/(1+t2)
    sy = 2*ty/(1+t2)
    sz = 2*tz/(1+t2)
 
-!Rotate the result with half magnetic impulse
+   !Rotate the result with half magnetic impulse
    ux_pr = ux_min + (uy_min*tz - uz_min*ty)
    uy_pr = uy_min - (ux_min*tz - uz_min*tx)
    uz_pr = uz_min + (ux_min*ty - uy_min*tx)
 
-!Rotate the result with full magnetic impulse
+   !Rotate the result with full magnetic impulse
    ux_plus = ux_min + (uy_min*sz - uz_min*sy)
    uy_plus = uy_min - (ux_min*sz - uz_min*sx)
    uz_plus = uz_min + (ux_min*sy - uy_min*sx)
 
-! Add remaining electric impulse
+   !Add remaining electric impulse
+   ux = ux_plus + qom * Ex * dt/2
+   uy = uy_plus + qom * Ey * dt/2
+   uz = uz_plus + qom * Ez * dt/2
+   u2 = (ux**2 + uy**2 + uz**2)
+   gamma = (1 + u2)**0.5
 
-  ux = ux_plus + (q/me)* Ex*dt/2
-  uy = uy_plus
-  uz = uz_plus
-  u2 = (ux**2 + uy**2 + uz**2)
-  gamma = (1 + u2)**0.5
+   x = x + dt*ux/gamma
+   y = y + dt*uy/gamma
+   z = z + dt*uz/gamma
 
-  x = x + dt*ux/gamma
-  y = y + dt*uy/gamma
-  z = z + dt*uz/gamma
-
-	T(i) = i*dt
-	XX(i) = x
-	YY(i) = y
-	ZZ(i) = z
-
-	UUX(i)  = ux
-	UUY(i)  = uy
-	UUZ(i)  = uz
+   T(i) = i*dt
+   XX(i) = x
+   YY(i) = y
+   ZZ(i) = z
+   UUX(i)  = ux
+   UUY(i)  = uy
+   UUZ(i)  = uz
 
 end do
 
-! do i = 1, Nt
- !   print *, T(i)
-! end do
-
-deallocate( T, XX, YY, UUX, UUY, UUZ )
 end program BorisPusher
